548aba882dd69c67be53c84350594b6af77ac974
Bryan Lackey <bryan.lackey@gmail.com>
2015-07-05 16:41:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 // catch 404 and forward to error handler
